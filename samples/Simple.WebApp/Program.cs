9cd8f80d5520efddcf6c2e8bfd49454fdb255c1a
Authfix <3940727+Authfix@users.noreply.github.com>
2018-01-05 11:01:31 +0100

    chore(sample): create sample project
    Create the sample project

---
+++ samples/Simple.WebApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CityOs.FileServer.Simple.WebApp
+{
+    public class Program
+    {
+        /// <summary>
+        /// The main entry of the program
+        /// </summary>
+        /// <param name="args">The arguments</param>
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        /// <summary>
+        /// Build the web host
+        /// </summary>
+        /// <param name="args">The arguments to build the web host</param>
+        /// <returns></returns>
+        public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ samples/Simple.WebApp/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CityOs.FileServer.Simple.WebApp
+{
+    public class Startup
+    {
+        /// <summary>
+        /// Configure the application services
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        /// <summary>
+        /// Configure the request pipeline
+        /// </summary>
+        /// <param name="app">The application builder</param>
+        /// <param name="env">The hosting environment</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
