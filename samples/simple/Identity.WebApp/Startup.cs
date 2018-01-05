573e59290a9b89b67fe18bf0822e579e06a39599
Authfix <3940727+Authfix@users.noreply.github.com>
2018-01-05 14:46:40 +0100

    chore(samples): create identity project
    Create the identity project to work with the simple webapp

---
+++ samples/simple/Identity.WebApp/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.WebApp
+{
+    public class Startup
+    {
+        /// <summary>
+        /// Configure the application services
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Configuration.GetApiResources())
+                .AddInMemoryClients(Configuration.GetClients());
+        }
+
+        /// <summary>
+        /// Configure the request pipeline
+        /// </summary>
+        /// <param name="app">The application builder</param>
+        /// <param name="env">The hosting environment</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+}
