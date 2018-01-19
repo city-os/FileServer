b0e9406e444200be60417fd209e392c350b4925d
Authfix <3940727+Authfix@users.noreply.github.com>
2018-01-19 12:05:20 +0100

    feat(claims): add security check
    Add security check for users.

--- src/Distributed.Mvc/Security/ReadAuthorizationHandler.cs
+++ src/Distributed.Mvc/Security/ReadAuthorizationHandler.cs
@@ -7,7 +7,15 @@ namespace CityOs.FileServer.Distributed.Mvc.Security
     {
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ReadAuthorizationRequirement requirement)
         {
-            context.Succeed(requirement);
+            if (context.User != null && context.User.Identity.IsAuthenticated)
+            {
+                var hasWriteClaim = context.User.HasClaim(c => c.Type == "scope" && (c.Value == "fileserver:read" || c.Value == "fileserver:read_write"));
+
+                if (hasWriteClaim)
+                {
+                    context.Succeed(requirement);
+                }
+            }
 
             return Task.CompletedTask;
         }
--- src/Distributed.Mvc/Security/WriteAuthorizationHandler.cs
+++ src/Distributed.Mvc/Security/WriteAuthorizationHandler.cs
@@ -7,6 +7,16 @@ namespace CityOs.FileServer.Distributed.Mvc.Security
     {
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, WriteAuthorizationRequirement requirement)
         {
+            if (context.User != null && context.User.Identity.IsAuthenticated)
+            {
+                var hasWriteClaim = context.User.HasClaim(c => c.Type == "scope" && c.Value == "fileserver:read_write");
+
+                if (hasWriteClaim)
+                {
+                    context.Succeed(requirement);
+                }
+            }
+
             return Task.CompletedTask;
         }
     }
