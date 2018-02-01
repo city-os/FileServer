809c78d88a7f3d55c2261f0b8c2003f082af65b1
Authfix <3940727+Authfix@users.noreply.github.com>
2018-02-01 18:09:49 +0100

    chore(app): create unit tests
    Create unit tests.

---
+++ tests/FileServer/AppService/ImagesAppServiceTests.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using CityOs.FileServer.AppService;
+using CityOs.FileServer.AppService.Adapters;
+using CityOs.FileServer.Dto;
+using CityOs.FileServer.Infrastructure.Repositories;
+using CityOs.FileServer.Tests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace CityOs.FileServer.Tests.AppService
+{
+    [TestClass]
+    public class ImageAppServiceTests
+    {
+        /// <summary>
+        /// Initializes the class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        [ClassInitialize]
+        public static void InitializeClass(TestContext context)
+        {
+            Mapper.Initialize(config => config.AddProfile<FileServerProfile>());
+        }
+
+        [TestMethod]
+        public async Task Should_GetImage_When_QueryHaveRightInformations()
+        {
+            const string fileName = "Rioji.png";
+
+            var appService = GetDefaultImageAppService();
+
+            var fileInformation = await appService.GetStreamByFileNameAsync(fileName, GetDefaultImageQueryDto());
+
+            Assert.IsNotNull(fileInformation);
+            Assert.AreEqual(fileInformation.OriginalFileName, fileName);
+            Assert.AreEqual(fileInformation.FileType, "image/png");
+            Assert.IsNotNull(fileInformation.Stream);
+        }
+
+        [TestMethod]
+        public async Task Should_ReturnNull_When_FileNotExists()
+        {
+            const string fileName = "Tata.jpeg";
+
+            var appService = GetDefaultImageAppService();
+
+            var fileInformation = await appService.GetStreamByFileNameAsync(fileName, GetDefaultImageQueryDto());
+
+            Assert.IsNull(fileInformation);
+        }
+
+        /// <summary>
+        /// Get a default <see cref="ImageAppService"/>
+        /// </summary>
+        /// <returns></returns>
+        private ImageAppService GetDefaultImageAppService()
+        {
+            var fileServerProvider = new MockFileServerProvider();
+            var repository = new ImageRepository(fileServerProvider);
+            var appService = new ImageAppService(repository, Mapper.Instance);
+
+            return appService;
+        }
+
+        /// <summary>
+        /// Gets a default image query data transfert object
+        /// </summary>
+        /// <returns></returns>
+        private ImageQueryDto GetDefaultImageQueryDto()
+        {
+            return new ImageQueryDto
+            {
+                Width = 100,
+                Height = 100
+            };
+        }
+    }
+}
---
+++ tests/FileServer/Mocks/MockFileServerProvider.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using CityOs.FileServer.Provider.Core;
+
+namespace CityOs.FileServer.Tests.Mocks
+{
+    internal class MockFileServerProvider : IFileServerProvider
+    {
+        public Task DeleteFileAsync(string fileName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> FileExists(string fileName)
+        {
+            var stream = GetEmbeddedFileStream(fileName);
+
+            if(stream != null)
+            {
+                stream.Dispose();
+                return Task.FromResult(true);
+            }
+
+            return Task.FromResult(false);
+        }
+
+        public Task<Stream> GetFileByIdentifierAsync(string fileName)
+        {
+            return Task.FromResult(GetEmbeddedFileStream(fileName));
+        }
+
+        public Task WriteFileAsync(Stream fileStream, string fileName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private Stream GetEmbeddedFileStream(string fileName)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var resource = string.Format("CityOs.FileServer.Tests.Data.{0}", fileName);
+
+            var stream = executingAssembly.GetManifestResourceStream(resource);
+            
+            return stream;
+        }
+    }
+}
