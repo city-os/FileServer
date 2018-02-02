d824fad3830095dd279a170489f48a5bbde464d8
Authfix <3940727+Authfix@users.noreply.github.com>
2018-02-02 14:12:03 +0100

    tests(appService): add tests for image resize
    Check if resize works correctly

--- tests/FileServer/AppService/ImagesAppServiceTests.cs
+++ tests/FileServer/AppService/ImagesAppServiceTests.cs
@@ -4,7 +4,9 @@ using CityOs.FileServer.AppService.Adapters;
 using CityOs.FileServer.Dto;
 using CityOs.FileServer.Infrastructure.Repositories;
 using CityOs.FileServer.Tests.Mocks;
+using ImageSharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CityOs.FileServer.Tests.AppService
@@ -12,6 +14,8 @@ namespace CityOs.FileServer.Tests.AppService
     [TestClass]
     public class ImageAppServiceTests
     {
+        public const string FileName = "Rioji.png";
+
         /// <summary>
         /// Initializes the class.
         /// </summary>
@@ -25,14 +29,53 @@ namespace CityOs.FileServer.Tests.AppService
         [TestMethod]
         public async Task Should_GetImage_When_QueryHaveRightInformations()
         {
-            const string fileName = "Rioji.png";
-
             var appService = GetDefaultImageAppService();
+            var imageQuery = GetDefaultImageQueryDto();
 
-            var fileInformation = await appService.GetStreamByFileNameAsync(fileName, GetDefaultImageQueryDto());
+            var fileInformation = await appService.GetStreamByFileNameAsync(FileName, imageQuery);
+
+            IsImageValid(fileInformation.Stream, imageQuery.Height, imageQuery.Width);
 
             Assert.IsNotNull(fileInformation);
-            Assert.AreEqual(fileInformation.OriginalFileName, fileName);
+            Assert.AreEqual(fileInformation.OriginalFileName, FileName);
+            Assert.AreEqual(fileInformation.FileType, "image/png");
+            Assert.IsNotNull(fileInformation.Stream);
+        }
+
+        [TestMethod]
+        public async Task Should_GetImage_When_QueryOnlyHeight()
+        {
+            var appService = GetDefaultImageAppService();
+
+            var imageQuery = GetDefaultImageQueryDto();
+            imageQuery.Width = 0;
+            imageQuery.Height = 20;
+
+            var fileInformation = await appService.GetStreamByFileNameAsync(FileName, imageQuery);
+
+            IsImageValid(fileInformation.Stream, imageQuery.Height, imageQuery.Width);
+
+            Assert.IsNotNull(fileInformation);
+            Assert.AreEqual(fileInformation.OriginalFileName, FileName);
+            Assert.AreEqual(fileInformation.FileType, "image/png");
+            Assert.IsNotNull(fileInformation.Stream);
+        }
+
+        [TestMethod]
+        public async Task Should_GetImage_When_QueryOnlyWidth()
+        {
+            var appService = GetDefaultImageAppService();
+
+            var imageQuery = GetDefaultImageQueryDto();
+            imageQuery.Width = 300;
+            imageQuery.Height = 0;
+
+            var fileInformation = await appService.GetStreamByFileNameAsync(FileName, imageQuery);
+
+            IsImageValid(fileInformation.Stream, imageQuery.Width, null);
+
+            Assert.IsNotNull(fileInformation);
+            Assert.AreEqual(fileInformation.OriginalFileName, FileName);
             Assert.AreEqual(fileInformation.FileType, "image/png");
             Assert.IsNotNull(fileInformation.Stream);
         }
@@ -70,9 +113,37 @@ namespace CityOs.FileServer.Tests.AppService
         {
             return new ImageQueryDto
             {
-                Width = 100,
-                Height = 100
+                Width = 640,
+                Height = 480
             };
+        }
+
+        /// <summary>
+        /// Check if the image is valid
+        /// </summary>
+        /// <param name="stream">The stream to validate</param>
+        /// <param name="height">The height</param>
+        /// <param name="width">The width</param>
+        /// <returns></returns>
+        private bool IsImageValid(Stream stream, int? height, int? width)
+        {
+            using (var image = Image.Load(stream))
+            {
+                bool heightValid = true;
+                bool widthValid = true;
+
+                if (height.HasValue)
+                {
+                    heightValid = image.Height == height.Value;
+                }
+
+                if (width.HasValue)
+                {
+                    widthValid = image.Width == width.Value;
+                }
+
+                return heightValid && widthValid;
+            }
         }
     }
 }
