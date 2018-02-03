c95b0460474a21e1886e17b4e1ed11b425c73492
Authfix <3940727+Authfix@users.noreply.github.com>
2018-02-03 14:39:56 +0100

    chore(app): move ImageAppService interface
    Move ImageAppService interface to it's own file.

---
+++ src/AppService/Images/IImageAppService.cs
@@ -0,0 +1,30 @@
+﻿using CityOs.FileServer.Dto;
+using System.Threading.Tasks;
+
+namespace CityOs.FileServer.AppService
+{
+    public interface IImageAppService
+    {
+        /// <summary>
+        /// Delete an image asynchronously
+        /// </summary>
+        /// <param name="fileName">The filename to delete</param>
+        /// <returns></returns>
+        Task DeleteImageAsync(string fileName);
+
+        /// <summary>
+        /// Gets stream function of a file name async
+        /// </summary>
+        /// <param name="fileName">The file name</param>
+        /// <param name="imageQuery">The image query</param>
+        /// <returns></returns>
+        Task<FileInformationDto> GetStreamByFileNameAsync(string fileName, ImageQueryDto imageQuery);
+        
+        /// <summary>
+        /// Save a file asynchronously
+        /// </summary>
+        /// <param name="fileInformationDto">The file information data transfert object</param>
+        /// <returns></returns>
+        Task<SavedImageDto> SaveImageAsync(FileInformationDto fileInformationDto);
+    }
+}
--- src/AppService/Images/ImagesAppService.cs
+++ src/AppService/Images/ImagesAppService.cs
@@ -50,29 +50,4 @@ namespace CityOs.FileServer.AppService
             return new SavedImageDto { RelativeUrl = savedImageUrl };
         }
     }
-
-    public interface IImageAppService
-    {
-        /// <summary>
-        /// Delete an image asynchronously
-        /// </summary>
-        /// <param name="fileName">The filename to delete</param>
-        /// <returns></returns>
-        Task DeleteImageAsync(string fileName);
-
-        /// <summary>
-        /// Gets stream function of a file name async
-        /// </summary>
-        /// <param name="fileName">The file name</param>
-        /// <param name="imageQuery">The image query</param>
-        /// <returns></returns>
-        Task<FileInformationDto> GetStreamByFileNameAsync(string fileName, ImageQueryDto imageQuery);
-        
-        /// <summary>
-        /// Save a file asynchronously
-        /// </summary>
-        /// <param name="fileInformationDto">The file information data transfert object</param>
-        /// <returns></returns>
-        Task<SavedImageDto> SaveImageAsync(FileInformationDto fileInformationDto);
-    }
 }
