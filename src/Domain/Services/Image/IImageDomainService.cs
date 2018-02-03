839f9d628e11d5dcd40b20cbccce6d67b8eedecf
Authfix <3940727+Authfix@users.noreply.github.com>
2018-02-03 14:33:18 +0100

    fix(domain): move image interface
    Move image interface to it's own file.

---
+++ src/Domain/Services/Image/IImageDomainService.cs
@@ -0,0 +1,47 @@
+﻿using CityOs.FileServer.Domain.Entities;
+
+namespace CityOs.FileServer.Domain.Services
+{
+    public interface IImageDomainService
+    {
+        /// <summary>
+        /// Check if we can try to use thumbnail or not
+        /// </summary>
+        /// <param name="imageQuery">The image query</param>
+        /// <returns></returns>
+        bool UseThumbnail(ImageQuery imageQuery);
+
+        /// <summary>
+        /// Check if we can try to use thumbnail or not
+        /// </summary>
+        /// <param name="height">The image height</param>
+        /// <param name="width">The image width</param>
+        /// <returns></returns>
+        bool UseThumbnail(int height, int width);
+
+        /// <summary>
+        /// Define if we need to generate a thumbnail for this image
+        /// </summary>
+        bool GenerateThumbnail(int height, int width);
+
+        /// <summary>
+        /// Gets the file thumbnail name
+        /// </summary>
+        /// <param name="fileName">The file name (with extension)</param>
+        /// <returns></returns>
+        string GetFileThumbnailName(string fileName);
+
+        /// <summary>
+        /// Check if we need to resize or not
+        /// </summary>
+        /// <param name="imageQuery">The image query</param>
+        /// <returns></returns>
+        bool ShouldResize(ImageQuery imageQuery, int maxHeight, int maxWidth);
+
+        /// <summary>
+        /// Gets the default thumbnail size
+        /// </summary>
+        /// <returns></returns>
+        ImageQuery GetDefaultThumbnailSize();
+    }
+}
